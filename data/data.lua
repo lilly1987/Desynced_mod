3c41d712436cbcec7a770694a8c6b28716af988f
lilly <lilly1987@naver.com>
2026-03-08 03:02:30 +0900

    1

---
+++ data/data.lua
@@ -0,0 +1,11 @@
+local package = ...
+
+-- files loaded when mod is initializing
+package.includes = {
+	"items.lua",
+	"frames.lua", 
+}
+
+-- called when mod is initializing
+function package:init()
+end
